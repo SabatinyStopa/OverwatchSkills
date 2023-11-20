b769b49e123f8b0da8cef110a72eac0cd13c4c59
sabatiny <sabatinystopa@gmail.com>
2023-11-20 18:45:01 -0300

    Adding samples for the input system and for the animations

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstPersonController : MonoBehaviour
+namespace OverwatchClone.Player
 {
-    // Start is called before the first frame update
-    void Start()
+    public class FirstPersonController : MonoBehaviour
     {
-        
+        [SerializeField] private float mouseSensitive = 100f;
+        [SerializeField] private float moveSpeed = 5f;
+
+        private SimpleControls controls;
+        private Vector2 rotation;
+
+        private void Awake() => controls = new SimpleControls();
+        private void OnEnable() => controls.Enable();
+        private void OnDisable() => controls.Disable();
+
+        private void Update()
+        {
+            RotationHandler();
+            MovimentHandler();
+        }
+
+        private void MovimentHandler()
+        {
+            var direction = controls.gameplay.move.ReadValue<Vector2>();
+
+            if (direction.sqrMagnitude < 0.01) return;
+
+            var scaledMoveSpeed = moveSpeed * Time.deltaTime;
+            var move = Quaternion.Euler(0, transform.eulerAngles.y, 0) * new Vector3(direction.x, 0, direction.y);
+            transform.position += move * scaledMoveSpeed;
+        }
+
+        private void RotationHandler()
+        {
+            var look = controls.gameplay.look.ReadValue<Vector2>();
+            if (look.sqrMagnitude < 0.01) return;
+
+            var scaledRotateSpeed = mouseSensitive * Time.deltaTime;
+
+            rotation.y += look.x * scaledRotateSpeed;
+            rotation.x = Mathf.Clamp(rotation.x - look.y * scaledRotateSpeed, -89, 89);
+            transform.localEulerAngles = rotation;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
